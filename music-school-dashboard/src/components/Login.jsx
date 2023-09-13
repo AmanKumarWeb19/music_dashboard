63cfa64356d6d488f5512187483c5acdfb7533ed
AmanKumarWeb19 <akgupta0524@gmail.com>
2023-09-13 12:24:50 +0530

    home page

--- music-school-dashboard/src/components/Login.jsx
+++ music-school-dashboard/src/components/Login.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+// src/Login.js
+import React from "react";
 
-const Login = () => {
+function Login() {
   return (
-    <div className=''>Login</div>
-  )
+    <div className="min-h-screen flex items-center justify-center bg-gray-200">
+      <div className="bg-white p-8 rounded-lg shadow-lg w-96">
+        <h2 className="text-2xl font-semibold mb-4">Login</h2>
+        <form>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="username"
+            >
+              Username
+            </label>
+            <input
+              className="border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
+              id="username"
+              type="text"
+              placeholder="Username"
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              className="border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:border-blue-500"
+              id="password"
+              type="password"
+              placeholder="Password"
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              Sign In
+            </button>
+            <a
+              className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+              href="#"
+            >
+              Forgot Password?
+            </a>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
